a7ef92dfbdd460d1bd36c3f56a27a11784683f8d
Tomás Alexandre de Moura Meneses <tomas.meneses@vectracs.com.br>
2019-06-14 08:28:53 -0300

    aaa

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,33 +1,49 @@
 import {expect} from 'chai';
-import { sum, sub, mult, div } from '../src/main'
+import { sum, sub, mult, div } from '../src/main';
 
 
+describe('Smoke Tests', () => {
+    it('should be a function ´sum´', () => {
+        expect(sum).to.be.a('function');
+    });
 
-describe('Calc',()=>{
-        
+    it('should be a function ´sub´', () => {
+        expect(sub).to.be.a('function');
+    });
+
+    it('should be a function ´mult´', () => {
+        expect(mult).to.be.a('function');
+    });
+
+    it('should be a function ´div´', () => {
+        expect(div).to.be.a('function');
+    });
+});
+
+
+describe('Calculator',()=>{
+
         describe('Sum', () => {
             it('should return 4 when sum(2,2)', () => {
                 expect(sum(2,2)).to.be.equal(4);
-            })
-        })
-        describe('sub', () => {
+            });
+        });
+
+        describe('Sub', () => {
             it('should return 4 when sub(2,2)', () => {
                 expect(sub(2,2)).to.be.equal(0);
-            })
-        })
-        describe('div', () => {
+            });
+        });
+
+        describe('Div', () => {
             it('should return 4 when div(2,2)', () => {
                 expect(div(2,2)).to.be.equal(1);
-            })
-        })
-        describe('mult', () => {
-            it('should be a function', () => {
-                expect(mult).to.be.a('function');
-            })
+            });
+        });
+
+        describe('Mult', () => {
             it('should return 4 when mult(2,2)', () => {
                 expect(mult(2,2)).to.be.equal(4);
-            })
+            });
         });
-    
-
 }); 
